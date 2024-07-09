c3c42b65941d6c7ffd04d9282c4e1777c87ed36d
Ravi Bhaskar <robinkr1249@gmail.com>
2024-07-09 13:07:29 +0530

    Initial commit

---
+++ FLASK/app.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template, url_for
+import pickle
+# import os
+# print(os.getcwd())  # Print current working directory
+# print(os.listdir())  # Print a list of files in the current working directory
+
+app=Flask(__name__)
+model=pickle.load(open('hr.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+@app.route('/Prediction',methods=['POST','GET'])
+def prediction():
+    return render_template('index.html')
+@app.route('/Home',methods=['POST','GET'])
+def my_home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def submit():
+    age = request.form['age']
+    experience = request.form['experience']
+    variables = [[int(age), int(experience)]]
+    result = model.predict(variables)
+    fin = int(result)
+
+    return render_template('result.html', salary=fin)
+
+
+if __name__=='__main__':
+    app.run(debug=False)
